302f6a5d85b021568284904fc5f2f80c04052027
pk-218 <2182001pk@gmail.com>
2021-06-20 23:13:43 +0530

    Added logging in Flask app

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,13 +1,16 @@
+import logging 
 from flask import Flask, json
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info("Main request successful")
     return "Hello World!"
 
 @app.route("/status")
 def status():
-    data = {"user": "admin"}
+    data = {"result": "OK - healthy"}
     response = app.response_class(
         response=json.dumps(data),
         status=200,
@@ -26,4 +29,5 @@ def metrics():
     return response
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
     app.run(host='0.0.0.0')
